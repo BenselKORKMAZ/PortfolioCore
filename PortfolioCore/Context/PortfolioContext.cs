d781a01bd5b1b0befaf50ff4f2455565dc5d8c18
Bensel <benselkorkmaz@gmail.com>
2025-04-22 20:08:25 +0100

    add

--- PortfolioCore/Context/PortfolioContext.cs
+++ PortfolioCore/Context/PortfolioContext.cs
@@ -9,12 +9,14 @@ namespace PortfolioCore.Context
 		{
 			optionsBuilder.UseSqlServer("Server=BESSOT;initial Catalog=PortfolioNightDb;integrated security=true;");
 		}
-
 		public DbSet<Contact> Contacts { get; set; }
 		public DbSet<Education> Educations { get; set; }
 		public DbSet<Experience> Experiences { get; set; }
 		public DbSet<Message> Messages { get; set; }
 		public DbSet<Testimonial> Testimonials { get; set; }
 		public DbSet<Skill> Skills { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Portfolio> Portfolios { get; set; }
+
 	}
 }
